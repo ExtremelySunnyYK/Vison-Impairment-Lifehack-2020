6072d8739328f9dc6530c6a12300b926da371875
Yong Kang <yongkang_chia@mymail.sutd.edu.sg>
2020-07-26 11:04:22 +0800

    added full stack

--- app.py
+++ app.py
@@ -10,13 +10,38 @@ from tensorflow.python.keras.preprocessing.image import ImageDataGenerator, img_
 from flask import request
 from flask import jsonify
 from flask import Flask
-from examples import predict_simplified_score
+from predict import *
 
 app = Flask(__name__)
 
 
 def get_score(left_eye,right_eye):
+    """
+    Returns AREDS Score given image
+    :Parameters : Image of eyes
+    
+    :returns : AREDS Score
+    """"
     score = predict_simplified_score.flask_predict()
     print("* Model loaded!")
 
     return score
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    message = request.get_json(force=True)
+    encoded = message['image']
+    decoded = base64.b64decode(encoded)
+    left_eye = Image.open(io.BytesIO(decoded))
+    right_eye = Image.open(io.BytesIO(decoded))
+    print(" * Loading Keras model...")
+    score = get_score(left_eye,right_eye)
+    # can add other features as well
+    # such as drusen size ...
+
+    response = {
+        'AREDS': {
+            'Level': score,
+        }
+    }
+    return jsonify(response)
